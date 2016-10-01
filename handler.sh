f6c47f87087ec268e1747c52e816d5b11829a098
vlna <vlna@users.noreply.github.com>
2016-10-01 16:03:24 +0200

    Update handler.sh

--- handler.sh
+++ handler.sh
@@ -3,7 +3,7 @@
 # Script to launch a web m3u or pls playlist (web radio) on mpd using mpc
 # based on code from https://bbs.archlinux.org/viewtopic.php?id=119903
 
-RADIO=`sed -n -e ${1}p radio.list`
+RADIO=`sed -n -e ${1}p /home/radio/radio.list`
 
 # Extract file extension
 
@@ -11,17 +11,21 @@ EXT=`echo "${RADIO##*.}" | tr A-Z a-z`
 
 case "$EXT" in
           "pls"   ).
-          PL=`curl $RADIO | grep '^File[0-9]*' | sed -e 's/^File[0-9]*=//'`
+          PL=`curl --post301 -L $RADIO | grep '^File[0-9]*' | sed -e 's/^File[0-9]*=//'`
           ;;
 
           "m3u" )
-          PL=`curl $RADIO | sed -e '/^#/D' | sed "s/\r$//" | head -n1`
+          PL=`curl --post301 -L $RADIO | sed -e '/^#/D' | sed "s/\r$//" | head -n1`
           ;;
 
           * )
           echo "Filename without a valid extension"
           exit 1;;
 esac
+
+# Store new channel for next/prev button
+
+echo ${1} > /tmp/irchan.tmp
 
 # Clear old playlist, add new playlist and play
 
